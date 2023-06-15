002fb244c8a6ce62e9d2863d71b654360a784f25
Emanuelle <emanuellercm@gmail.com>
2023-06-15 08:12:00 -0300

    feat: context/provider creation and use of the data in the Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useContext } from "react";
+import { CounterContext } from "../context/CounterContext";
 
 const Home = () => {
+  // desestruturando pra consumir aqui na Home
+  // o valor do contexto compartilhado em CounterContext.js
+  const { counter } = useContext(CounterContext);
   return (
     <div>
-      <p>Context api</p>
+      <h1>Home</h1>
+      <p>Valor do contador: {counter}</p>
     </div>
   );
 };
